96c23318181df685bd1bf3ff7b35ec1e6c6d1ac3
Butters <swki418@gmail.com>
2025-06-01 16:14:17 +0800

    feat: update styles and extend color palette in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,39 @@ export default {
   theme: {
     extend: {
       colors: {
-        'my-primary': 'rgba(61 182 211,2)',
+        primary: {
+          DEFAULT: '#ffaa00',
+          light: '#ffbb33',
+          dark: '#cc8800',
+        },
+        secondary: {
+          DEFAULT: '#f4a7b9',
+          light: '#f8c4d1',
+          dark: '#d37d90',
+        },
+        warning: {
+          DEFAULT: '#d3381c',
+          light: '#e25c4f',
+          dark: '#a82c16',
+        },
+        info: {
+          DEFAULT: '#58a6dc',
+          light: '#7fbce6',
+          dark: '#357bb3',
+        },
+        success: {
+          DEFAULT: '#91c788',
+          light: '#afd4aa',
+          dark: '#6f9f66',
+        },
+        background: {
+          DEFAULT: '#f9f5ef',
+          paper: '#ffffff',
+        },
+        text: {
+          primary: '#333333',
+          secondary: '#666666',
+        },
       },
     },
   },
